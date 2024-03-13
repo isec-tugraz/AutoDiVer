b832fdee82f40ca8bb365c7db642837cfb3478dd
Shibam Ghosh <shibam.math@gmail.com>
2024-03-13 22:09:16 +0200

    added gift 128

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     ext_modules = cythonize([
         # Extension("differential_verification.skinny.skinny", ["src/differential_verification/skinny/skinny.pyx"]),
         Extension("differential_verification.gift64.gift_cipher", ["src/differential_verification/gift64/gift_cipher.pyx"]),
+         Extension("differential_verification.gift128.gift_cipher", ["src/differential_verification/gift128/gift_cipher.pyx"]),
         Extension("differential_verification.midori64.midori_cipher", ["src/differential_verification/midori64/midori_cipher.pyx"]),
         Extension("differential_verification.midori128.midori_cipher", ["src/differential_verification/midori128/midori_cipher.pyx"]),
     ]),
