8475213ddbd47ee104be695d505658a15680b566
Marcel Nageler <marcel.nageler@iaik.tugraz.at>
2024-03-15 10:52:54 +0100

    add missing things to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import Extension, setup, find_packages
 from Cython.Build import cythonize
 setup(
     ext_modules = cythonize([
-        # Extension("differential_verification.skinny.skinny", ["src/differential_verification/skinny/skinny.pyx"]),
+        Extension("differential_verification.skinny.skinny", ["src/differential_verification/skinny/skinny.pyx"]),
         Extension("differential_verification.gift64.gift_cipher", ["src/differential_verification/gift64/gift_cipher.pyx"]),
-         Extension("differential_verification.gift128.gift_cipher", ["src/differential_verification/gift128/gift_cipher.pyx"]),
+        Extension("differential_verification.gift128.gift_cipher", ["src/differential_verification/gift128/gift_cipher.pyx"]),
         Extension("differential_verification.midori64.midori_cipher", ["src/differential_verification/midori64/midori_cipher.pyx"]),
         Extension("differential_verification.midori128.midori_cipher", ["src/differential_verification/midori128/midori_cipher.pyx"]),
     ]),
