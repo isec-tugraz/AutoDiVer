6145058f180420c17d4aa3713c15cac5a31e56fd
Shibam Ghosh <shibam.math@gmail.com>
2024-04-02 22:20:59 +0300

    added warp

--- src/differential_verification/warp128/warp128_model.py
+++ src/differential_verification/warp128/warp128_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.
 """
 model the solutions of a differential characteristic for GIFT64 and count them.
 """
@@ -7,7 +7,7 @@ import logging
 import numpy as np
 from typing import Any
 from sat_toolkit.formula import XorCNF
-from .util import DDT, RC, perm_nibble_16, perm_nibble_16_inv
+from .util import DDT, RC, perm_nibble_16, perm_nibble_16_inv, perm_nibble_inv
 from ..cipher_model import SboxCipher, DifferentialCharacteristic
 log = logging.getLogger(__name__)
 class WARP128(SboxCipher):
@@ -18,8 +18,6 @@ class WARP128(SboxCipher):
     key_size = 128
     sbox_bits = 4
     sbox_count = 16
-    key: np.ndarray[Any, np.dtype[np.int32]]
-    mc_out: np.ndarray[Any, np.dtype[np.int32]]
     def __init__(self, char: DifferentialCharacteristic):
         super().__init__(char)
         self.char = char
@@ -39,39 +37,62 @@ class WARP128(SboxCipher):
         self._model_sboxes()
         self._model_linear_layer()
     def _create_vars(self):
-        self.add_index_array('key', (2, self.sbox_count, self.sbox_bits))
+        self.add_index_array('key', (2*self.sbox_count, self.sbox_bits))
         self.add_index_array('sbox_in', (self.num_rounds, self.sbox_count, self.sbox_bits))
         self.add_index_array('sbox_out', (self.num_rounds, self.sbox_count, self.sbox_bits))
-        self.add_index_array('X', (1, self.sbox_count, self.sbox_bits))
+        self.add_index_array('X', (self.sbox_count, self.sbox_bits))
+        self.add_index_array('Y', (self.sbox_count, self.sbox_bits))
         self.add_index_array('tweak', (0,))
-        # self.pt = np.empty((2*self.sbox_count, 4))
+        self.pt = self.get_round_var(self.sbox_in[0], self.X)
+        self._fieldnames.add('pt')
+        #self.rounds_in = np.empty((self.num_rounds, 2*self.sbox_count, self.sbox_bits))
+        #self.rounds_in[0] = self.get_round_var(self.sbox_in[0], self.X[0])
+        #for i in range(1, self.num_rounds):
+        #    self.rounds_in[i] = self.get_round_var(self.sbox_in[i], perm_nibble_16(self.sbox_in[i-1]))
+        #self.rounds_out = np.empty((self.num_rounds, 2*self.sbox_count, self.sbox_bits))
+        #for i in range(0, self.num_rounds-1):
+        #    self.rounds_out[i] = perm_nibble_inv(self.rounds_in[i])
+        ##no permutation at the end
+        #self.rounds_out[self.num_rounds-1] = self.get_round_var(self.sbox_in[self.num_rounds-1], self.Y[0])
+        # np.empty((2*self.sbox_count, 4), dtype=np.uint32)
         # for i in range(self.sbox_count):
         #     self.pt[2*i] = self.sbox_in[0][i]
         #     self.pt[2*i+1] = self.X[0][i]
-        # self._fieldnames.add('pt')
         # print(self.sbox_in[0])
         # print(self.X)
         # print(self.pt)
+    def get_round_var(self, a, b):
+        v = np.empty((2*self.sbox_count, 4), dtype=np.uint32)
+        for i in range(self.sbox_count):
+            v[2*i] = a[i]
+            v[2*i+1] = b[i]
+        return v
     def linear_layer(self, sbox_out, key, xor_in, xor_out, RC0, RC1):
-        temp = xor_out.copy()
-        xor_out_flat = temp.reshape(-1) # don't use .flatten() here because it creates a copy
+        temp = xor_in.copy()
+        xor_in_flat = temp.reshape(-1) # don't use .flatten() here because it creates a copy
         # flip bits according to round constant
-        xor_out_flat[0] *= (-1)**(RC0 & 0x1)
-        xor_out_flat[1] *= (-1)**((RC0 >> 1) & 0x1)
-        xor_out_flat[2] *= (-1)**((RC0 >> 2) & 0x1)
-        xor_out_flat[3] *= (-1)**((RC0 >> 3) & 0x1)
-        xor_out_flat[4] *= (-1)**(RC1 & 0x1)
-        xor_out_flat[5] *= (-1)**((RC1 >> 1) & 0x1)
-        xor_out_flat[6] *= (-1)**((RC1 >> 2) & 0x1)
-        xor_out_flat[7] *= (-1)**((RC1 >> 3) & 0x1)
+        xor_in_flat[0] *= (-1)**(RC0 & 0x1)
+        xor_in_flat[1] *= (-1)**((RC0 >> 1) & 0x1)
+        xor_in_flat[2] *= (-1)**((RC0 >> 2) & 0x1)
+        xor_in_flat[3] *= (-1)**((RC0 >> 3) & 0x1)
+        xor_in_flat[4] *= (-1)**(RC1 & 0x1)
+        xor_in_flat[5] *= (-1)**((RC1 >> 1) & 0x1)
+        xor_in_flat[6] *= (-1)**((RC1 >> 2) & 0x1)
+        xor_in_flat[7] *= (-1)**((RC1 >> 3) & 0x1)
         lin_cnf = XorCNF()
-        lin_cnf += XorCNF.create_xor(xor_out_flat, xor_in.flatten(), sbox_out.flatten(), key.flatten())
+        lin_cnf += XorCNF.create_xor(xor_in_flat, xor_out.flatten(), sbox_out.flatten(), key.flatten())
         self.cnf += lin_cnf
     def _model_linear_layer(self):
+        key = self.key.copy().reshape(2, 16, 4)
         for r in range(self.num_rounds):
             if r == 0:
-                xor_in = self.X[0].copy()
+                xor_in = self.X.copy()
             else:
                 xor_in = perm_nibble_16(self.sbox_in[r-1])
-            xor_out = perm_nibble_16_inv(self.sbox_in[r])
-            self.linear_layer(self.sbox_out[r], self.key[r%2], xor_in, xor_out, RC[0][r], RC[1][r])
+            if r == (self.num_rounds - 1):
+                xor_out = self.Y.copy()
+            else:
+                xor_out = perm_nibble_16_inv(self.sbox_in[r+1])
+            # print(xor_in)
+            # print(xor_out)
+            self.linear_layer(self.sbox_out[r], key[r%2], xor_in, xor_out, RC[0][r], RC[1][r])
--- tests/test_warp.py
+++ tests/test_warp.py
@@ -6,6 +6,10 @@ from differential_verification.warp128.warp128_model import WARP128
 from differential_verification.warp128.warp_cipher import warp_enc
 from sat_toolkit.formula import CNF
 from icecream import ic
+def print_state(key):
+    for k in key:
+        print(hex(k)[2:], end = " ")
+    print("")
 # midori64_testvectors = [
 #     (0x0000000000000000, 0x00000000000000000000000000000000, 0x3c9cceda2bbd449a),
 #     (0x42c20fd3b586879e, 0x687ded3b3c85b3f35b1009863e2a8cbf, 0x66bcdc6270d901cd),
@@ -20,6 +24,51 @@ from icecream import ic
 #     print(f'{ct = :016x}')
 #     print(f'{ct ^ ct_ref = :016x}')
 #     assert ct == ct_ref
+perm = [31, 6, 29, 14, 1, 12, 21, 8, 27, 2, 3, 0, 25, 4, 23, 10, 15, 22, 13, 30, 17, 28, 5, 24, 11, 18, 19, 16, 9, 20, 7, 26]
+def perm_nibble_inv(temp):
+    state = [0 for i in range(32)]
+    for j in range(32):
+        state[j] = temp[perm[j]]
+    return np.asarray(state, np.uint8)
+def perm_nibble_16(state):
+    state1 = [0 for i in range(32)]
+    for i in range(16):
+        state1[2*i] = state[i]
+    temp = [0 for i in range(32)]
+    for i in range(32):
+        temp[perm[i]] = state1[i]
+    state2 = [0 for i in range(16)]
+    for i in range(16):
+        state2[i] = temp[2*i + 1]
+    return np.asarray(state2, np.uint8)
+def perm_nibble_16_inv(state):
+    state1 = [0 for i in range(32)]
+    for i in range(16):
+        state1[2*i+1] = state[i]
+    temp = [0 for i in range(32)]
+    for i in range(32):
+        temp[i] = state1[perm[i]]
+    state2 = [0 for i in range(16)]
+    for i in range(16):
+        state2[i] = temp[2*i]
+    return np.asarray(state2, np.uint8)
+def get_round_val(a, b):
+    v = np.empty(32, dtype=np.uint8)
+    for i in range(16):
+        v[2*i] = a[i]
+        v[2*i+1] = b[i]
+    return v
+def get_round_in_out(rounds, model):
+    rounds_in = np.empty((rounds, 32), dtype=np.uint8)
+    rounds_out = np.empty((rounds, 32), dtype=np.uint8)
+    for i in range(0, rounds-1):
+        rounds_out[i] = get_round_val(model.sbox_in[i], perm_nibble_16_inv(model.sbox_in[i+1]))
+    #no permutation at the end
+    rounds_out[rounds-1] = get_round_val(model.sbox_in[rounds-1], model.Y)
+    # rounds_in[0] = get_round_val(model.sbox_in[0], model.X)
+    # for i in range(1, rounds):
+    #     rounds_in[i] = get_round_val(model.sbox_in[i], perm_nibble_16(model.sbox_in[i-1]))
+    return rounds_out
 def test_zero_characteristic():
     numrounds = 2
     sbi = sbo = np.zeros((numrounds, 16), dtype=np.uint8)
@@ -31,22 +80,37 @@ def test_zero_characteristic():
     # print(warp.cnf)
     num_solutions = count_solutions(warp.cnf, epsilon=0.8, delta=0.2, verbosity=0)
     assert num_solutions == 1 << (128 + 128)
-    # for bit_var in warp.key.flatten():
-    #     warp.cnf += CNF([bit_var * (-1)**randint(0,1), 0])
-    # num_solutions = count_solutions(warp.cnf, epsilon=0.8, delta=0.2, verbosity=0)
-    # assert num_solutions == 1 << 128
-    # for bit_var in warp.sbox_in[0].flatten():
-    #     warp.cnf += CNF([bit_var * (-1)**randint(0,1), 0])
-    # model = warp.solve()
-    # key = model.key # type: ignore
-    # sbi = model.sbox_in # type: ignore
-    # sbo = model.sbox_out # type: ignore
-    # assert np.all(warp.sbox[sbi[:warp.num_rounds]] == sbo)
-    # for r, round_sbi in enumerate(sbi):
-    #     ref = warp128_enc(sbi[0], key, r)
-    #     assert np.all(round_sbi == ref)
-    # num_solutions = count_solutions(warp.cnf, epsilon=0.8, delta=0.2, verbosity=0)
-    # assert num_solutions == 1
+    for bit_var in warp.key.flatten():
+        warp.cnf += CNF([bit_var * (-1)**randint(0,1), 0])
+    num_solutions = count_solutions(warp.cnf, epsilon=0.8, delta=0.2, verbosity=0)
+    assert num_solutions == 1 << 128
+    for bit_var in warp.sbox_in[0].flatten():
+        warp.cnf += CNF([bit_var * (-1)**randint(0,1), 0])
+    model = warp.solve()
+    key = model.key # type: ignore
+    print_state(key)
+    pt = model.pt # type: ignore
+    sbi = model.sbox_in # type: ignore
+    sbo = model.sbox_out # type: ignore
+    assert np.all(warp.sbox[sbi[:warp.num_rounds]] == sbo)
+    rout = get_round_in_out(numrounds, model)
+    # print_state(rin[0])
+    # assert np.all(rin[0] == pt)
+    print_state(pt)
+    for r, round_out in enumerate(rout):
+        ref = warp_enc(pt, key, r+1)
+        print("----------------------------------")
+        print_state(sbi[r])
+        if r != warp.num_rounds - 1:
+            print_state(perm_nibble_16_inv(sbi[r+1]))
+        else:
+            print_state(model.Y)
+        print_state(ref)
+        print_state(round_out)
+        print("----------------------------------")
+        assert np.all(round_out == ref)
+    num_solutions = count_solutions(warp.cnf, epsilon=0.8, delta=0.2, verbosity=0)
+    assert num_solutions == 1
 # def test_nonzero_characteristic():
 #     # characteristic from https://doi.org/10.1109/ACCESS.2020.2995795S (Figure 3)
 #     # with alpha = beta = 1
