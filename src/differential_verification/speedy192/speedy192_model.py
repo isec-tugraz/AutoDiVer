b6d487065d6f82f1734342f60d7ea83b1d22c2f8
Shibam Ghosh <shibam.math@gmail.com>
2024-05-17 17:26:10 +0300

    added speedy model

---
+++ src/differential_verification/speedy192/speedy192_model.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+"""
+model the solutions of a differential characteristic for GIFT64 and count them.
+"""
+from __future__ import annotations
+import logging
+import numpy as np
+from typing import Any
+from sat_toolkit.formula import XorCNF
+from .ddt import DDT
+from .util import SBOX, RC, do_shift_cols, update_key
+from ..cipher_model import SboxCipher, DifferentialCharacteristic
+log = logging.getLogger(__name__)
+class Speedy192(SboxCipher):
+    cipher_name = "SPEEDY192"
+    sbox = SBOX.copy()
+    # print(f'{sbox}')
+    ddt  = DDT
+    block_size = 192
+    key_size = 192
+    sbox_bits = 6
+    sbox_count = 32
+    # key: np.ndarray[Any, np.dtype[np.int32]]
+    # mc_out: np.ndarray[Any, np.dtype[np.int32]]
+    def __init__(self, char: DifferentialCharacteristic):
+        super().__init__(char)
+        self.char = char
+        self.num_rounds = char.num_rounds # this is actually number of sboxes
+        assert char.num_rounds%2 == 0 #nimber of sbox layers should be even
+        self.num_rounds_full = char.num_rounds//2
+        assert self.char.sbox_in.shape == self.char.sbox_out.shape
+        if self.char.sbox_in.shape != self.char.sbox_out.shape:
+            raise ValueError('sbox_in.shape must equal sbox_out.shape')
+        self._create_vars()
+        self._key_schedule()
+        self._model_sboxes()
+        self._model_sc()  #ShiftColumn
+        self._model_add_key()
+        self._model_linear_layer()
+    def _create_vars(self):
+        self.add_index_array('key', (1, self.sbox_count, self.sbox_bits))
+        self.add_index_array('sbox_in', (self.num_rounds, self.sbox_count, self.sbox_bits))
+        self.add_index_array('sbox_out',(self.num_rounds, self.sbox_count, self.sbox_bits))
+        self.add_index_array('mc_out', (self.num_rounds_full - 1, self.sbox_count, self.sbox_bits))
+        self.add_index_array('tweak', (0,))
+        self.pt = self.sbox_in[0]
+        self._fieldnames.add('pt')
+    def _key_schedule(self) -> None:
+        RK = []
+        rk = self.key[0].copy()
+        # print(rk)
+        for i in range(self.num_rounds_full):
+            rk = update_key(rk)
+            RK.append(rk)
+        self._round_keys = np.array(RK)
+    def _model_sc(self):
+        for i in range(self.num_rounds_full):
+            """
+            Note that speedy has two ShiftColumn in each full round. But
+            we model only one of then which is between two Sboxes. The other
+            ShiftColumn applied during MixColumn operation
+            """
+            scin_flat = do_shift_cols(self.sbox_out[2*i]).flatten()
+            scout_flat = self.sbox_in[2*i+1].flatten()
+            sc_cnf = XorCNF.create_xor(scin_flat, scout_flat)
+            self.cnf += sc_cnf
+    def _addKey(self, Y, X, K, RC: np.ndarray):
+        X_flat = X.copy().reshape(32, 6)
+        for i in range(32):
+            X_flat[i][0]  *= (-1)**(RC[i] & 0x1)
+            X_flat[i][1]  *= (-1)**((RC[i]>>1) & 0x1)
+            X_flat[i][2]  *= (-1)**((RC[i]>>2) & 0x1)
+            X_flat[i][3]  *= (-1)**((RC[i]>>3) & 0x1)
+            X_flat[i][4]  *= (-1)**((RC[i]>>4) & 0x1)
+            X_flat[i][5]  *= (-1)**((RC[i]>>5) & 0x1)
+        X_flat = X_flat.flatten()
+        key_xor_cnf = XorCNF.create_xor(X_flat, Y.flatten(), K.flatten())
+        return key_xor_cnf
+    def _model_add_key(self):
+        for r in range(self.num_rounds_full -1):
+            #No need to model AddKey for first and last round
+            self.cnf += self._addKey(self.mc_out[r], self.sbox_in[2*(r+1)], self._round_keys[r], RC[r])
+            # self.cnf += self._addKey(self.mc_out[r], self.sbox_in[2*(r+1)], self.key, RC[r])
+    @staticmethod
+    def model_mix_cols(A, B):
+        alphas = [0, 1, 5, 9, 15, 21, 26]
+        mc_cnf = XorCNF()
+        for c in range(6):
+            colA = A[:, c]
+            colB = B[:, c]
+            # print(f'{colA}')
+            for r in range(32):
+                colA_red = np.empty(len(alphas), dtype=np.uint32)
+                for i in range(len(alphas)):
+                    colA_red[i] = colA[(r + alphas[i])%32]
+                # print(f'{colB[r]}', "===>", f'{colA_red}')
+                mc_cnf += XorCNF.create_xor([colB[r]], *colA_red)
+        return mc_cnf
+    def _model_linear_layer(self):
+        for r in range(self.num_rounds_full - 1):
+            mc_input = do_shift_cols(self.sbox_out[2*r + 1])
+            mc_output = self.mc_out[r].copy()
+            self.cnf += self.model_mix_cols(mc_input, mc_output)
---
+++ tests/test_speedy192.py
@@ -0,0 +1,110 @@
+from random import seed, randint
+from differential_verification.cipher_model import DifferentialCharacteristic, count_solutions
+from differential_verification.speedy192.speedy192_model import Speedy192
+from differential_verification.speedy192.speedy_cipher import speedy192_enc
+from differential_verification.speedy192.util import Add, prepare_round_keys
+import numpy as np
+import pytest
+from sat_toolkit.formula import CNF
+from icecream import ic
+#0th bit is the LSB
+def print_state(key, s = "state"):
+    print(s, end = ": " )
+    for k in key:
+        # print(k)
+        assert k <= 64
+        print(hex(k)[2:].zfill(2), end = " ")
+    print("")
+# speedy192_testvectors = [
+#     ("51084ce6e73a5ca2ec87d7babc297543", "687ded3b3c85b3f35b1009863e2a8cbf", "1e0ac4fddff71b4c1801b73ee4afc83d"),
+# ]
+# speedy192_testvectors = [
+#     (bytes.fromhex(pt), bytes.fromhex(key), bytes.fromhex(ct_ref)) for pt, key, ct_ref in speedy192_testvectors
+# ]
+# @pytest.mark.parametrize("pt,key,ct_ref", speedy192_testvectors)
+# def test_tv(pt: bytes, key: bytes, ct_ref: bytes):
+#     print(f'{pt=}')
+#     print(f'{key=}')
+#     print(f'{ct_ref=}')
+#     ct = speedy192_enc(pt, key, 7)
+#     assert ct == ct_ref
+def test_zero_characteristic():
+    seed("test_speedy192::test_zero_characteristic")
+    numrounds = 2
+    sbi_delta = sbo_delta = np.zeros((2*numrounds, 32), dtype=np.uint8)
+    char = DifferentialCharacteristic(sbi_delta, sbo_delta)
+    speedy = Speedy192(char)
+    # num_solutions = count_solutions(speedy.cnf, epsilon=0.8, delta=0.2, verbosity=0)
+    # assert num_solutions == 1 << (192 + 192)
+    for bit_var in speedy.key.flatten():
+        speedy.cnf += CNF([bit_var * (-1)**randint(0,1), 0])
+        # speedy.cnf += CNF([-bit_var, 0])
+    # num_solutions = count_solutions(speedy.cnf, epsilon=0.8, delta=0.2, verbosity=0)
+    # assert num_solutions == 1 << 192
+    for bit_var in speedy.sbox_out[0, 1:].flatten():
+        speedy.cnf += CNF([bit_var * (-1)**randint(0,1), 0])
+        # speedy.cnf += CNF([-bit_var, 0])
+    for bit_var in speedy.sbox_out[0, :1].flatten():
+        speedy.cnf += CNF([bit_var * (-1)**randint(0,1), 0])
+        # speedy.cnf += CNF([bit_var, 0])
+    model = speedy.solve()
+    key = model.key[0] # type: ignore
+    sbi = model.sbox_in # type: ignore
+    sbo = model.sbox_out # type: ignore
+    mco = model.mc_out # type: ignore
+    assert np.all(speedy.sbox[sbi[:speedy.num_rounds]] == sbo)
+    print_state(key)
+    pt = Add(sbi[0], key)
+    print_state(pt)
+    round_keys = prepare_round_keys(key)
+    for i in range(speedy.num_rounds_full):
+        print_state(round_keys[i], "rkeys{}".format(i))
+    #Note that inside the model ther is not key addition at the start and end
+    ref = speedy192_enc(pt, key, numrounds)
+    ref = Add(ref, round_keys[numrounds])
+    out = sbo[2*(numrounds-1) + 1]
+    print_state(ref, "ref")
+    print_state(out, "sbo")
+    assert np.all(ref == out)
+    num_solutions = count_solutions(speedy.cnf, epsilon=0.8, delta=0.2, verbosity=0)
+    assert num_solutions == 1
+def test_nonzero_characteristic():
+    char =( (( 1,  0,  0,  0,  1,  1,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  1,  0,  0,  0),
+             (16,  0,  0,  0, 16, 16,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0, 16,  0,  0,  0)),
+            (( 0,  0,  0, 16, 16,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0, 16,  0,  0,  0, 16),
+             ( 0,  0,  0,  4,  4,  0,  0,  0,  0,  0,  4,  0,  0,  0,  0,  4,  0,  0,  0,  0,  0,  4,  0,  0,  0,  0,  0,  4,  0,  0,  0,  4)),
+            (( 0,  4,  0,  4,  0,  0,  0,  0,  0,  0,  0,  0,  0,  4,  0,  4,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  4),
+             ( 0, 16,  0, 16,  0,  0,  0,  0,  0,  0,  0,  0,  0, 17,  0, 34,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 16)),
+            ((16,  0, 16,  0,  0,  0,  0,  0,  1,  0,  0,  2, 16,  0,  0, 32,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  2,  0,  0,  0,  0),
+             ( 4,  0,  4,  0,  0,  0,  0,  0,  4,  0,  0,  4,  4,  0,  0,  4,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  4,  0,  0,  0,  0)) )
+    numrounds = 1  #Number of full rounds
+    sbi_delta = np.array([[x for x in in_out[0]] for in_out in char], dtype=np.uint8)
+    sbo_delta = np.array([[x for x in in_out[1]] for in_out in char], dtype=np.uint8)
+    char = DifferentialCharacteristic(sbi_delta, sbo_delta)
+    speedy = Speedy192(char)
+    model = speedy.solve()
+    key = model.key[0] # type: ignore
+    sbi = model.sbox_in # type: ignore
+    sbo = model.sbox_out # type: ignore
+    mco = model.mc_out # type: ignore
+    assert np.all(speedy.sbox[sbi[:speedy.num_rounds]] == sbo)
+    print_state(key)
+    pt = Add(sbi[0], key)
+    print_state(pt)
+    # ref = speedy192_enc(pt, key, numrounds)
+    # for r in range(numrounds):
+    #     print_state(sbi[2*r], "sbi")
+    #     print_state(sbo[2*r], "sbo")
+    #     print_state(sbi[2*r+1],"sbi")
+    #     print_state(sbo[2*r+1],"sbo")
+    #     if r != (numrounds - 1):
+    #         print_state(mco[r],    "mco")
+    # out = sbo[2*(numrounds-1) + 1]
+    # print_state(ref, "ref")
+    # print_state(out, "sbo")
+    # assert np.all(ref == out)
+    # num_solutions = count_solutions(speedy.cnf, epsilon=0.8, delta=0.2, verbosity=0)
+    # assert num_solutions == 1
+if __name__ == "__main__":
+    test_zero_characteristic()
+    test_nonzero_characteristic()
