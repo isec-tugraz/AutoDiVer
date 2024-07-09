ebd201c65bfed034e20a0f03c2bab9fd655b8ec6
Marcel Nageler <marcel.nageler@iaik.tugraz.at>
2024-07-09 12:16:10 +0200

    finalize PRESENT model

--- src/autodiver/present/present_model.py
+++ src/autodiver/present/present_model.py
@@ -4,17 +4,15 @@ model the solutions of a differential characteristic for GIFT64 and count them.
 """
 from __future__ import annotations
 import logging
-import numpy as np
 from typing import Any
-from sat_toolkit.formula import XorCNF
+from pathlib import Path
 
 from .present_cipher import s_box, p_layer_order
+from .present_util import PERM, INV_PERM, bit_perm
 from ..cipher_model import SboxCipher, DifferentialCharacteristic
 
-
-# get permutation based on source indices (instead of target indices)
-PERM = np.zeros(64, dtype=int)
-PERM[p_layer_order] = np.arange(64)
+import numpy as np
+from sat_toolkit.formula import XorCNF
 
 
 log = logging.getLogger(__name__)
@@ -37,6 +35,38 @@ PRESENT_DDT = np.array(
      [ 0,  4,  0,  0,  4,  0,  0,  0,  0,  0,  0,  0,  0,  0,  4,  4]])
 
 
+class PresentCharacteristic(DifferentialCharacteristic):
+    @classmethod
+    def load(cls, characteristic_path: Path) -> DifferentialCharacteristic:
+        with np.load(characteristic_path) as f:
+            sbox_in = f['sbox_in']
+            sbox_out = f['sbox_out']
+        sbox_in = np.array(sbox_in, dtype=np.uint8)
+        sbox_out = np.array(sbox_out, dtype=np.uint8)
+
+        if sbox_in.shape != sbox_out.shape:
+            raise ValueError('sbox_in and sbox_out must have the same shape')
+        if len(sbox_in.shape) != 2 or len(sbox_out.shape) != 2:
+            raise ValueError('sbox_in and sbox_out must have 2 dimensions')
+        if sbox_in.shape[1] != 16 or sbox_out.shape[1] != 16:
+            raise ValueError('sbox_in and sbox_out must have 16 s-boxes')
+
+        num_rounds = sbox_in.shape[0]
+        permuted = bit_perm(sbox_out)
+
+        for i in range(1, num_rounds):
+            lin_input = sbox_out[i - 1]
+            lin_output = sbox_in[i]
+            permuted = bit_perm(lin_input)
+            if not np.all(permuted == lin_output):
+                raise ValueError(f'linear layer condition violated at sbox_out[{i - 1}] -> sbox_in[{i}]')
+
+
+        if sbox_in.shape != sbox_out.shape:
+            raise ValueError('sbox_in and sbox_out must have the same shape')
+        return cls(sbox_in, sbox_out, file_path=characteristic_path)
+
+
 class Present(SboxCipher):
     sbox = np.array([int(x, 16) for x in "c56b90ad3ef84712"], dtype=np.uint8)
     ddt  = PRESENT_DDT
@@ -48,6 +78,9 @@ class Present(SboxCipher):
     round_keys: np.ndarray[Any, np.dtype[np.int32]]
 
     def __init__(self, char: DifferentialCharacteristic, **kwargs):
+        if not isinstance(char, PresentCharacteristic):
+            raise ValueError('char must be a PresentCharacteristic')
+
         super().__init__(char, **kwargs)
         self.char = char
         self.num_rounds = char.num_rounds
@@ -92,14 +125,20 @@ class Present(SboxCipher):
     def _fmt_arr(cls, arr: np.ndarray, cellsize: int):
         if cellsize == 0 and len(arr) == 0:
             return ''
+
         if cellsize == 4:
-            return ''.join(f'{x:01x}' for x in arr.flatten())
-        if cellsize == 8:
-            return ''.join(f'{x:02x}' for x in arr.flatten())
-        if cellsize == 64:
-            return ''.join(f'{x:016x}' for x in arr.flatten())
+            int_val = sum(int(x) << i * 4 for i, x in enumerate(arr))
+            return f'0x{int_val:016x}'
+
+        assert np.all(arr < 256) and np.all(arr > 0)
+        int_val = sum(int(x) << i * 8 for i, x in enumerate(arr))
         if cellsize == 80:
-            return ''.join(f'{x:020}' for x in arr.flatten())
+            return f'0x{int_val:020x}'
+        if cellsize == 128:
+            return f'0x{int_val:032x}'
+
+        return f'0x{int_val:0{len(arr // 4)}x}'
+
         raise ValueError(f'cellsize must be 4 or 8 not {cellsize}')
 
     def applyPerm(self, array: np.ndarray[Any, np.dtype[np.int32]]) -> np.ndarray[Any, np.dtype[np.int32]]:
