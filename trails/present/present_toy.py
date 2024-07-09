aa478b3345451ac122500b57a00eb878ecde7ec3
Marcel Nageler <marcel.nageler@iaik.tugraz.at>
2024-07-09 14:26:46 +0200

    add toy example for present

---
+++ trails/present/present_toy.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+Toy example for present with 8 active s-boxes in 2 rounds
+"""
+from pathlib import Path
+import numpy as np
+
+from present_util import bit_perm
+
+ddt = np.array([[16,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+                [ 0,  0,  0,  4,  0,  0,  0,  4,  0,  4,  0,  0,  0,  4,  0,  0],
+                [ 0,  0,  0,  2,  0,  4,  2,  0,  0,  0,  2,  0,  2,  2,  2,  0],
+                [ 0,  2,  0,  2,  2,  0,  4,  2,  0,  0,  2,  2,  0,  0,  0,  0],
+                [ 0,  0,  0,  0,  0,  4,  2,  2,  0,  2,  2,  0,  2,  0,  2,  0],
+                [ 0,  2,  0,  0,  2,  0,  0,  0,  0,  2,  2,  2,  4,  2,  0,  0],
+                [ 0,  0,  2,  0,  0,  0,  2,  0,  2,  0,  0,  4,  2,  0,  0,  4],
+                [ 0,  4,  2,  0,  0,  0,  2,  0,  2,  0,  0,  0,  2,  0,  0,  4],
+                [ 0,  0,  0,  2,  0,  0,  0,  2,  0,  2,  0,  4,  0,  2,  0,  4],
+                [ 0,  0,  2,  0,  4,  0,  2,  0,  2,  0,  0,  0,  2,  0,  4,  0],
+                [ 0,  0,  2,  2,  0,  4,  0,  0,  2,  0,  2,  0,  0,  2,  2,  0],
+                [ 0,  2,  0,  0,  2,  0,  0,  0,  4,  2,  2,  2,  0,  2,  0,  0],
+                [ 0,  0,  2,  0,  0,  4,  0,  2,  2,  2,  2,  0,  0,  0,  2,  0],
+                [ 0,  2,  4,  2,  2,  0,  0,  2,  0,  0,  2,  2,  0,  0,  0,  0],
+                [ 0,  0,  2,  2,  0,  0,  2,  2,  2,  2,  0,  0,  2,  2,  0,  0],
+                [ 0,  4,  0,  0,  4,  0,  0,  0,  0,  0,  0,  0,  0,  0,  4,  4]])
+
+
+if __name__ == '__main__':
+    nrounds = 2
+    sbox_in = np.zeros((nrounds, 16), dtype=np.uint8)
+    sbox_out = np.zeros((nrounds, 16), dtype=np.uint8)
+
+    sbox_in[0, [15, 14, 13, 12]] = [1, 2, 4, 8]
+    sbox_out[0, [15, 14, 13, 12]] = [3, 5, 5, 11]
+
+    sbox_in[1] = bit_perm([sbox_out[0]])[0]
+    sbox_out[1] = np.argmax(ddt[sbox_in[1], :], axis=1)
+
+
+    print("".join(f"{x:x}" for x in reversed(sbox_in[0])))
+    print("".join(f"{x:x}" for x in reversed(sbox_out[0])))
+    print("".join(f"{x:x}" for x in reversed(sbox_in[1])))
+    print("".join(f"{x:x}" for x in reversed(sbox_out[1])))
+
+    # sbox_in[3, [0, 3]] = 4
+    
+    prob = np.log2(ddt[sbox_in, sbox_out] / 16).sum()
+    print(f"probability: 2^{prob:.1f}")
+
+    script_file = Path(__file__)
+    dst_file = script_file.with_suffix('.npz')
+    print(f'Writing to {dst_file}')
+    np.savez(dst_file, sbox_in=sbox_in, sbox_out=sbox_out)
+    # numrounds = len(sbox_in)
---
+++ trails/present/present_util.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+p_layer_order = np.array([
+    0, 16, 32, 48, 1, 17, 33, 49,
+    2, 18, 34, 50, 3, 19, 35, 51,
+    4, 20, 36, 52, 5, 21, 37, 53,
+    6, 22, 38, 54, 7, 23, 39, 55,
+    8, 24, 40, 56, 9, 25, 41, 57,
+    10, 26, 42, 58, 11, 27, 43, 59,
+    12, 28, 44, 60, 13, 29, 45, 61,
+    14, 30, 46, 62, 15, 31, 47, 63
+])
+
+
+# get permutation based on source indices (instead of target indices)
+PERM = np.zeros(64, dtype=int)
+PERM[p_layer_order] = np.arange(64)
+
+INV_PERM = np.array(p_layer_order, dtype=int)
+
+def unpack_bits(arr: np.ndarray):
+    arr = np.array(arr, dtype=np.uint8)
+    bits = np.unpackbits(arr, axis=-1, bitorder='little')
+    bit_offsets = [0, 1, 2, 3]
+    selector = np.stack([np.arange(16) * 8 + o for o in bit_offsets]).T.flatten()
+    bits = bits[..., selector]
+    return bits
+
+
+def pack_bits(bits: np.ndarray):
+    bit_offsets = [0, 1, 2, 3]
+    selector = np.stack([np.arange(16) * 8 + o for o in bit_offsets]).T.flatten()
+    padded_bits = np.zeros((len(bits), 128), dtype=np.uint8)
+    padded_bits[..., selector] = bits
+    return np.packbits(padded_bits, axis=1, bitorder='little')
+
+
+def inverse_bit_perm(arr):
+    bits = unpack_bits(arr)
+    ip = bits[..., INV_PERM]
+    res = pack_bits(ip)
+    return res
+
+
+def bit_perm(arr):
+    bits = unpack_bits(arr)
+    pemuted = bits[..., PERM]
+    res = pack_bits(pemuted)
+    return res
