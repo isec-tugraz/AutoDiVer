00c0d515a76a98dc4afb10efc73c8fb55fa9262a
Marcel Nageler <marcel.nageler@iaik.tugraz.at>
2024-07-24 13:50:01 +0200

    improve run_docker.sh script

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -euo pipefail
+
+# chdir to script location
+cd "$(dirname "$0")"
+
 docker build -t autodiver .
 docker run -v .:/mnt  --rm -it autodiver /bin/bash -c 'cd /mnt; exec bash'
